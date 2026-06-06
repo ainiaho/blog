38d7f4fe3ee58423894a8f91711cc9836bb68340
aniaho <aniaho@users.noreply.github.com>
2026-06-06 02:41:22 +0000

    fix: restore missing let xml declaration in generateSitemap

--- scripts/build.js
+++ scripts/build.js
@@ -1060,6 +1060,7 @@ function generate404() {
 // Generate Sitemap
 function generateSitemap(posts) {
     const baseUrl = 'https://blog.diepthink.top';
+    let xml = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
 
     // Static pages
     ['', '/search.html'].forEach(page => {
